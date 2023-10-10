eb6f95ca1caa8bf00a2a5958c6bcb1ba3fb85fb1
farissbayu <bayustfa@gmail.com>
2023-10-10 15:12:03 +0700

    feat: simplify code

--- src/main/java/com/example/restapijfasttext/config/FastTextConfiguration.java
+++ src/main/java/com/example/restapijfasttext/config/FastTextConfiguration.java
@@ -5,10 +5,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.io.ClassPathResource;
-import org.springframework.core.io.Resource;
-
-import java.io.IOException;
 
 @Configuration
 @ComponentScan
--- src/main/java/com/example/restapijfasttext/service/ClassifyService.java
+++ src/main/java/com/example/restapijfasttext/service/ClassifyService.java
@@ -3,43 +3,16 @@ package com.example.restapijfasttext.service;
 import com.example.restapijfasttext.model.Classify;
 import com.github.jfasttext.JFastText;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.ApplicationContext;
 import org.springframework.stereotype.Service;
-
-//@Service
-//public class ClassifyService {
-//    static Classify classify = new Classify();
-//    static JFastText jft = new JFastText();
-//
-//    // load model
-//    public ClassifyService() {
-//        jft.loadModel("./src/test/resources/models/model_cooking.bin");
-//    }
-//
-//    // predict and return label text
-//    public Classify predictLabel(String text) {
-//        JFastText.ProbLabel probLabel = jft.predictProba(text);
-//        classify.setAll(text, probLabel.label, Math.exp(probLabel.logProb));
-//        return classify;
-//    }
-//}
 
 @Service
 public class ClassifyService {
-
-    private final ApplicationContext context;
-    private Classify classify = new Classify();
-
     @Autowired
-    public ClassifyService(ApplicationContext applicationContext) {
-        this.context = applicationContext;
-    }
+    private JFastText jFastText;
 
     public Classify predictLabel(String text) {
-        JFastText jFastText = context.getBean(JFastText.class);
         JFastText.ProbLabel label = jFastText.predictProba(text);
-
-        classify.setAll(text, label.label, Math.exp(label.logProb));
+        Classify classify = new Classify(text, label.label, Math.exp(label.logProb));
         return classify;
     }
 
