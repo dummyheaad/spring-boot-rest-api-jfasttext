1be058236df260ea567b198e6faa5262ccc0e4c2
Aysuka Ansari <aysukaansari@apps.ipb.ac.id>
2023-10-26 16:01:03 +0700

    fix script

--- build_push_image.sh
+++ build_push_image.sh
@@ -7,7 +7,7 @@ docker build -t tetanggaku/nlp-chat-service:v1 .
 docker tag tetanggaku/nlp-chat-service:v1 ghcr.io/dummyheaad/tetanggaku/nlp-chat-service:v1
 
 # login ke GitHub Packages dengan Personal Access Token
-echo $CR_PAT | docker login ghcr.io -u dummyheaad --password-stdin
+echo $GP_PAT | docker login ghcr.io -u dummyheaad --password-stdin
 
 # unggah image ke GitHub Packages
 docker push ghcr.io/dummyheaad/tetanggaku/nlp-chat-service:v1
