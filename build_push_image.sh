e6970e78389f8720a533500100b701921ef82962
Aysuka Ansari <aysukaansari@apps.ipb.ac.id>
2023-10-26 15:53:20 +0700

    add GitHub Package push script

---
+++ build_push_image.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# membuat docker image dari Dockerfile
+docker build -t tetanggaku/nlp-chat-service:v1 .
+
+# menyesuaikan nama image dengan format GitHub Packages
+docker tag item-app:v1 ghcr.io/dummyheaad/tetanggaku/nlp-chat-service:v1
+
+# login ke GitHub Packages dengan Personal Access Token
+echo $CR_PAT | docker login ghcr.io -u dummyheaad --password-stdin
+
+# unggah image ke GitHub Packages
+docker push ghcr.io/dummyheaad/tetanggaku/nlp-chat-service:v1
